8e9ee453baacf38e521e263da83125a67ea931d9
newhiman <newhiman@gmail.com>
2026-06-12 12:15:07 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WuxiaGame from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WuxiaGame />
+  </React.StrictMode>
+)
